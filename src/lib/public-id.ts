d775f0c1e5360a09cc8a0deebf8e6c8448caef95
nafees nazik <nafeesnazik21@gmail.com>
2023-12-19 22:49:52 +0530

    feat: add public id util

---
+++ src/lib/public-id.ts
@@ -0,0 +1,16 @@
+import { customAlphabet } from "nanoid";
+
+const alphabet = "0123456789abcdefghijklmnopqrstuvwxyz";
+const length = 12;
+
+const nanoid = customAlphabet(alphabet, length);
+
+const PREFIX = {
+  user: "usr",
+  workspace: "ws",
+  member: "mbr",
+} as const;
+
+export function generatePublicId(key: keyof typeof PREFIX) {
+  return `${PREFIX[key]}-${nanoid()}`;
+}
