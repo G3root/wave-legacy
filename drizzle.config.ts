5728d9cda302b40b6ec2c49ff6d43d854717d74b
nafees nazik <nafeesnazik21@gmail.com>
2023-12-19 22:27:00 +0530

    fix: drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,5 +1,7 @@
+import "dotenv/config";
 import type { Config } from "drizzle-kit";
-import { env } from "@/env.mjs";
+
+import { env } from "./src/env.mjs";
 
 export default {
   schema: "./src/server/db/schema/index.ts",
