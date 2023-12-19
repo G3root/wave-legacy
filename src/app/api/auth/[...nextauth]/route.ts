32741860448220cb176a999a1f09416bbf2fa27c
nafees nazik <nafeesnazik21@gmail.com>
2023-12-19 22:26:49 +0530

    fix: route

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,5 @@
-import { handlers } from "@/lib/next-auth/auth-options";
-export const { GET, POST } = handlers;
+import { NextAuthConfig } from "@/lib/next-auth/auth-options";
+import NextAuth from "next-auth";
+
+const handler = NextAuth(NextAuthConfig);
+export { handler as GET, handler as POST };
