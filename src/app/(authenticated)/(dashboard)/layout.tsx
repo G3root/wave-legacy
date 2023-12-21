7863985ba2c3597acaa10af454966bf61e99c058
nafees nazik <nafeesnazik21@gmail.com>
2023-12-21 22:17:11 +0530

    feat: add padding to layout

--- src/app/(authenticated)/(dashboard)/layout.tsx
+++ src/app/(authenticated)/(dashboard)/layout.tsx
@@ -12,7 +12,9 @@ export default async function DashboardLayout({
     <div className="bg-background">
       <div className="grid lg:grid-cols-5">
         <SideBar />
-        {children}
+        <div className="col-span-3 lg:col-span-4 lg:border-l">
+          <div className="h-full px-4 py-6 lg:px-8">{children}</div>
+        </div>
       </div>
     </div>
   );
